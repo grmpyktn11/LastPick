8e35718392da8d24de123b960fbb640b96a6d8b4
khalid <71997967+grmpyktn11@users.noreply.github.com>
2025-06-16 14:15:13 -0400

    Delete flaskApp/databaseInfo.py

--- flaskApp/databaseInfo.py
+++
@@ -1,7 +0,0 @@
-hostName = 'b4qjeynjbw52tfmzhzpt-mysql.services.clever-cloud.com'
-databaseName = "b4qjeynjbw52tfmzhzpt"
-databaseUsername = 'uvdt6xvgkzfhicng'
-databasePassword = "QFegIP10cqSKyzNsKRI8"
-databasePort = 3306
-databaseConnectionURI = "mysql://uvdt6xvgkzfhicng:QFegIP10cqSKyzNsKRI8@b4qjeynjbw52tfmzhzpt-mysql.services.clever-cloud.com:3306/b4qjeynjbw52tfmzhzpt"
-key = "hi"
